32b801b48802568adb69bb31cc13f2dfdd3ea48c
KimMundo01 <801sml@gmail.com>
2024-09-12 09:55:55 +0900

    setting

---
+++ Tripting/src/main/java/org/example/tripting/TriptingApplication.java
@@ -0,0 +1,13 @@
+package org.example.tripting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriptingApplication.class, args);
+    }
+
+}
---
+++ Tripting/src/test/java/org/example/tripting/TriptingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tripting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
