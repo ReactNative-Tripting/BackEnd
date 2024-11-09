da05f5a1782c0a9923c2dac833574d5f45ce14e5
KimMundo01 <801sml@gmail.com>
2024-11-09 16:22:08 +0900

    war 패키징

--- src/main/java/org/example/tripting/TriptingApplication.java
+++ src/main/java/org/example/tripting/TriptingApplication.java
@@ -2,12 +2,20 @@ package org.example.tripting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class TriptingApplication {
+public class TriptingApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TriptingApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // WAR 배포 시 메인 클래스를 설정
+        return application.sources(TriptingApplication.class);
     }
 
+    public static void main(String[] args) {
+        // JAR로 실행할 때는 이 메서드가 사용됨
+        SpringApplication.run(TriptingApplication.class, args);
+    }
 }
--- src/main/java/org/example/tripting/mission/Mission.java
+++ src/main/java/org/example/tripting/mission/Mission.java
@@ -2,8 +2,10 @@ package org.example.tripting.mission;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.example.tripting.place.Place;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 @Document(collection = "Mission")
 @Getter
@@ -12,6 +14,9 @@ public class Mission {
 
     @Id
     private String id; //ID
+
+    @DocumentReference
+    private Place adress;
 
     private Integer msDifficult; //미션 난이도
     private Integer point; //미션당 포인트
